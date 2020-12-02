bd3d3462e0eb9ffbd5eb7c30079efd53594a6aa6
Tanya Ermolaeva <tanyaermolaeva@MacBook-Air-Tanya.local>
2020-12-02 12:07:15 +0300

    Homework

--- src/HomeWork1/HomeWork4.java
+++ src/HomeWork1/HomeWork4.java
@@ -1,7 +1,7 @@
 package HomeWork1;
 public class HomeWork4 {
     public static void main(String[] args){
-        Boolean sleep = sleepIn (vacation); //???
+        Boolean sleep = sleepIn(true, false);
         System.out.println(sleep);
     }
     public static Boolean sleepIn (boolean weekday, boolean vacation){
