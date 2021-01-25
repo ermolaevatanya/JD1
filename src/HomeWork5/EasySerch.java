d081885c677783ba6ab05d55d7616a4a3836df93
Tanya Ermolaeva <tanyaermolaeva@MacBook-Air-Tanya.local>
2021-01-25 21:38:11 +0300

    ДЗ 5

---
+++ src/HomeWork5/EasySerch.java
@@ -0,0 +1,10 @@
+package HomeWork5;
+
+public class EasySerch implements ISearchEngine{
+    public long search (String text, String word){
+        int count = 0;
+
+        return count;
+    }
+
+}
