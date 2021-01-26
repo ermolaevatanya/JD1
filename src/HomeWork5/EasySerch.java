b5e95656b868178937b8ce6f7729a1bee5ba554c
Tanya Ermolaeva <tanyaermolaeva@MacBook-Air-Tanya.local>
2021-01-26 22:28:10 +0300

    ДЗ 5

--- src/HomeWork5/EasySerch.java
+++ src/HomeWork5/EasySerch.java
@@ -1,10 +1,17 @@
 package HomeWork5;
 
 public class EasySerch implements ISearchEngine{
-    public long search (String text, String word){
-        int count = 0;
-
-        return count;
+    public long search(String text, String word) {
+        long counter = 0;
+        word = " " + word + " ";
+        int index = -1;
+        do {
+            index = text.indexOf(word, index+1);
+            if(index != -1){
+                counter++;
+            }
+        } while (index != -1);
+        return counter;
     }
 
 }
